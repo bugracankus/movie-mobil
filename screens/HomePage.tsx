7f3de3109344121c443639c89f5e788a7e6f1196
Bugra Can Kus <bugracan_kus@hotmail.com>
2023-11-22 18:01:58 +0300

    Initial commit

---
+++ screens/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+const HomePage: React.FC = () => {
+    return (
+        <View >
+            <Text>Hello Word</Text>
+        </View>
+    )
+}
+export default HomePage;
